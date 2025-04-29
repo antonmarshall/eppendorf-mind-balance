da7418d0bd9aa7e6be1039837ce0646b5c7a2277
antonmarshall <anton.marshall03@gmail.com>
2025-04-29 11:35:22 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/eppendorf-mind-balance/',  // Replace with your repository name
+  base: '/eppendorf-mind-balance/',  // Match this with BrowserRouter basename
   server: {
     host: "::",
     port: 8080,
